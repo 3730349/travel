375db5ebd4c38d9ee311a660a3b40faf433f44da
3730349 <1627106767@qq.com>
2018-12-02 14:30:05 +0800

    add header

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+  <home-header></home-header>
+  </div>
+  
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+(function (doc, win) {
+        var docEl = doc.documentElement,
+            resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+            recalc = function () {
+                var clientWidth = docEl.clientWidth;
+                if (!clientWidth) return;
+                if(clientWidth>=640){
+                    docEl.style.fontSize = '100px';
+                }else{
+                    // docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+                    docEl.style.fontSize = 50 + 'px';
+                }
+            };
+
+        if (!doc.addEventListener) return;
+        win.addEventListener(resizeEvt, recalc, false);
+        doc.addEventListener('DOMContentLoaded', recalc, false);
+    })(document, window);
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="header">
+      <div class="header-left">
+          <div class="iconfont back-icon">&#xe682;</div>
+      </div>
+      <div class="header-input">
+          <span class="iconfont">&#xe643;</span>
+          输入城市/景点/游玩主题
+          </div>
+      <div class="header-right">
+          城市
+          <span class="iconfont">&#xe6aa;</span>
+          </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HoemHeader',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+    @import '~reset/varibles.styl';
+    *
+        padding 0
+        margin 0
+    .header
+        line-height .86rem
+        background red
+        display flex
+        font-size .3rem
+        background $bgColor
+        color #fff
+        .header-left
+            width .64rem
+            float left
+            .back-icon
+                text-align center
+                font-size .4rem
+
+        .header-input
+            flex 1
+            height .64rem
+            line-height .64rem
+            margin-top .12rem
+            margin-left .2rem
+            padding-left .2rem
+            background #ffffff
+            border-radius .1rem
+            color #ccc
+        .header-right
+            width 1.24rem
+            float right
+            text-align center
+
+
+</style>
