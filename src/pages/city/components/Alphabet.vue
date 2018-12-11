405a90a32e7a255ebd9908378eb9a0d0d27f829b
3730349 <1627106767@qq.com>
2018-12-11 17:26:59 +0800

    perfomaance

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,16 +1,70 @@
 <template>
     <ul class="list">
-       <li class="item">A</li>
-       <li class="item">A</li>
-       <li class="item">A</li>
-       <li class="item">A</li>
-
+       <li class="item" 
+       v-for="item of letters" 
+       :key="item"
+       :ref="item"
+       @touchstart='handleTouchStart'
+       @touchmove='handleTouchMove'
+       @touchend='handleTouchEnd'
+       @click="handleLetterClick">
+       {{item}}
+       </li>
     </ul>
 
 </template>
 <script>
 export default {
-    name: 'CityAlphabet'
+    name: 'CityAlphabet',
+    props: {
+        cities: Object
+    },
+    computed: {
+        letters () {
+            const letters = []
+            for(let i in this.cities) {
+                letters.push(i)
+            }
+            return letters
+        } 
+    },
+    data () {
+        return {
+                TouchStatus: false,
+                startY: 0,
+                timer: null
+
+        }
+    },
+    updated () {
+        this.startY = this.$refs['A'][0].offsetTop
+    },
+    methods: {
+        handleLetterClick (e) {
+            this.$emit('change',e.target.innerText)
+        },
+        handleTouchStart () {
+            this.TouchStatus = true
+        },
+        handleTouchMove (e) {
+            if(this.TouchStatus) {
+                if(this.timer) {
+                    clearTimeout(this.timer)
+                }
+                this.timer = setTimeout(() => {
+                    const touchY = e.touches[0].clientY - 104;
+                    const index = Math.floor((touchY - this.startY) / 19)
+                    if(index >= 0 && index < this.letters.length){
+                        this.$emit('change', this.letters[index])
+                    }
+                },16)
+
+            }
+        },
+        handleTouchEnd () {
+            this.TouchStatus = false
+        }
+    }
 }
 </script>
 <style lang='stylus' scoped>
@@ -20,15 +74,15 @@ export default {
         flex-direction column
         justify-content center
         position absolute
-        top 1.58rem
+        top 1.88rem
         right 0
         bottom 0
         width .4rem
         // background red
         .item
             text-align center
-            line-height .44rem
-            color $baColor
+            line-height .34rem
+            color $bgColor
 
 
 
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -12,58 +12,26 @@
             <div class="area">
                 <div class="title border-topbotom">热门城市</div>
                 <div class="botton-list">
-                    <div class="botton-wrapper">
-                        <div class="botton">北京</div>
-                    </div>
-                    <div class="botton-wrapper">
-                        <div class="botton">北京</div>
-                    </div>
-                    <div class="botton-wrapper">
-                        <div class="botton">北京</div>
-                    </div>
-                    <div class="botton-wrapper">
-                        <div class="botton">北京</div>
+                    <div class="botton-wrapper" 
+                    v-for="item of hot" 
+                    :key="item.id">
+                        <div class="botton">{{item.name}}</div>
                     </div>
                 </div>
             </div>
-            <div class="area">
-                <div class="title border-topbotom">A</div>
+            <div 
+                class="area" 
+                v-for="(item , key) of cities" 
+                :key='key'
+                :ref='key'
+                >
+                <div class="title border-topbotom">{{key}}</div>
                 <div class="item-list">
-                    <div class="item border-bottom">阿拉尔</div>
-                    <div class="item border-bottom">阿拉尔</div>
-                    <div class="item border-bottom">阿拉尔</div>
-                    <div class="item border-bottom">阿拉尔</div>
-                    <div class="item border-bottom">阿拉尔</div>
-                </div>
-            </div>
-            <div class="area">
-                <div class="title border-topbotom">A</div>
-                <div class="item-list">
-                    <div class="item border-bottom">阿拉尔</div>
-                    <div class="item border-bottom">阿拉尔</div>
-                    <div class="item border-bottom">阿拉尔</div>
-                    <div class="item border-bottom">阿拉尔</div>
-                    <div class="item border-bottom">阿拉尔</div>
-                </div>
-            </div>
-            <div class="area">
-                <div class="title border-topbotom">A</div>
-                <div class="item-list">
-                    <div class="item border-bottom">阿拉尔</div>
-                    <div class="item border-bottom">阿拉尔</div>
-                    <div class="item border-bottom">阿拉尔</div>
-                    <div class="item border-bottom">阿拉尔</div>
-                    <div class="item border-bottom">阿拉尔</div>
-                </div>
-            </div>
-            <div class="area">
-                <div class="title border-topbotom">A</div>
-                <div class="item-list">
-                    <div class="item border-bottom">阿拉尔</div>
-                    <div class="item border-bottom">阿拉尔</div>
-                    <div class="item border-bottom">阿拉尔</div>
-                    <div class="item border-bottom">阿拉尔</div>
-                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom" 
+                    v-for="innerItem of item" 
+                    :key="innerItem.id">
+                    {{innerItem.name}}
+                    </div>
                 </div>
             </div>
         </div>
@@ -73,9 +41,23 @@
 import Bscroll from 'better-scroll'
 export default {
     name: 'CityLit',
+    props: {
+        hot: Array,
+        cities: Object,
+        letter: String
+    },
     mounted () {
         this.scroll = new Bscroll(this.$refs.wrapper)
 
+    },
+    //监听， 点击右侧的字母时跳到相应的城市
+    watch: {
+        letter() {
+            if(this.letter) {
+                const element = this.$refs[this.letter][0]
+                this.scroll.scrollToElement(element)
+            }
+        }
     }
 }
 </script>
